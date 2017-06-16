9047a39c2b8b9ffa011e880c52f5cfa9a6d0e2ca
rakshit joshi <raxit2009@gmail.com>
2017-06-16 13:26:19 +0000

    change in env varialbe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #Use for email sending
+  config.action_mailer.default_url_options = { :host => ENV['MAILER_URL'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
